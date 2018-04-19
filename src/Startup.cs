e6f121742db3ca00e73bef3ad2132ec9f6f72ad3
Jz.Fan <jz.fan@qq.com>
2018-04-19 16:13:34 +0800

    add project

---
+++ src/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Gateway
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+			var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+	        builder.SetBasePath(env.ContentRootPath)
+		        .AddJsonFile($"appsettings.{env.EnvironmentName}.json")
+		        //add configuration.json
+		        .AddJsonFile($"configuration.{env.EnvironmentName}.json")
+		        .AddEnvironmentVariables();
+
+	        Configuration = builder.Build();
+		}
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot(Configuration);
+        }
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+	        app.UseStaticFiles();
+
+	        await app.UseOcelot();
+        }
+    }
+}
